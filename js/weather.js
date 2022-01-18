760b6a7d33116e1f1c0bed57223884af4b17b6eb
leekeonjong <asdzxc3232@naver.com>
2022-01-18 17:33:10 +0900

    321132213

--- js/weather.js
+++ js/weather.js
@@ -14,6 +14,9 @@ let am06temp = document.getElementById('am06temp');
 let am12temp = document.getElementById('am12temp');
 let pm06temp = document.getElementById('pm06temp');
 let pm09temp = document.getElementById('pm09temp');
+let dailyDate=document.getElementsByClassName('daily_date');
+let dailyTemp=document.getElementsByClassName('daily_temp');
+let dailyIcon=document.getElementsByClassName('daily_icon');
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
 function onGeoOk(position){
@@ -44,9 +47,54 @@ function onGeoOk(position){
     am12.innerText=data.list[2].dt_txt.substring(11,16);
     pm06.innerText=data.list[4].dt_txt.substring(11,16);
     pm09.innerText=data.list[5].dt_txt.substring(11,16);
+    dailyDate[0].innerText=data.list[9].dt_txt.substring(5,10);
+    dailyDate[1].innerText=data.list[17].dt_txt.substring(5,10);
+    dailyDate[2].innerText=data.list[25].dt_txt.substring(5,10);
+    dailyDate[3].innerText=data.list[33].dt_txt.substring(5,10);
+    dailyDate[4].innerText=data.list[39].dt_txt.substring(5,10);
+    dailyTemp[0].innerText=data.list[9].main.temp+'℃';
+    dailyTemp[1].innerText=data.list[17].main.temp+'℃';
+    dailyTemp[2].innerText=data.list[25].main.temp+'℃';
+    dailyTemp[3].innerText=data.list[33].main.temp+'℃';
+    dailyTemp[4].innerText=data.list[39].main.temp+'℃';
+    for(var i=0;i<5;i++){
+      let icon = data.list[8*i+7].weather[0].main;
+      if(icon.indexOf('Snow')!==-1){
+        dailyIcon[i].innerHTML='<i class="fas fa-snowflake"></i>';
+      } else if(icon.indexOf('Cloud')!==-1){
+        dailyIcon[i].innerHTML='<i class="fas fa-cloud"></i>';
+      } else if(icon.indexOf('Rain')!==-1){
+        dailyIcon[i].innerHTML='<i class="fas fa-cloud-showers-heavy"></i>';
+      } else {
+        dailyIcon[i].innerHTML='<i class="fas fa-sun"></i>';
+      }
+    }
+    // console.log(data.list[9].weather[0].main.match('Snow'));
   })
 }
 
 function onGeoError(){
   alert('cant workl');
 }
+
+function otherCity() {
+  let cityName = document.getElementById('searchcity').value;
+  const keyApi='09A376B6-886C-3877-8D72-B59F5AE68017'
+  const url1=`http://api.vworld.kr/req/address?service=address&request=getcoord&version=2.0&crs=epsg:4326&address=${cityName}&refine=true&simple=false&format=json&type=road&key=${keyApi}`
+  console.log(url1);
+  fetch(url1).then(response => response.json()).then(data=>{
+    console.log('das')
+  })
+  
+}
+
+
+function test() {
+  weatherSummary.innerText='dassdd'
+}
+
+function test2(){
+  navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+}
+
+
